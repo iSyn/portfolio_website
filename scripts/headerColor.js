f31609dfd13ea630b4206fb5242d04e9a2e7fd50
Synclair Wang <synclairwang11@yahoo.com>
2016-10-21 16:43:32 -0400

    added animate.css as well as more header shit

--- scripts/headerColor.js
+++ scripts/headerColor.js
@@ -4,17 +4,43 @@ $(() => {
 
 
   const changeColor = (e) => {
-    let color = $(e.target).data('color');
-    console.log(color);
-    $(e.target).css('color', color);
+    let color = $(e.target).data('color')
+    console.log(color)
+    $(e.target).css('color', color)
   }
 
-  const originalColor = (e) => {
-    $(e.target).css('color', '#3366CC');
+  const randomColor = (e) => {
+    const colorsILike = [
+      'deepskyblue',
+      'cyan',
+      'turquoise',
+      'mediumturquoise',
+      'aqua',
+      'darkturquoise',
+      'skyblue',
+      'lightskyblue',
+      'steelblue',
+      'dodgerblue',
+      'cornflowerblue',
+      'royalblue',
+      'midnightblue',
+      'navy',
+    ]
+    let colorChoice = Math.floor(Math.random() * colorsILike.length)
+    $(e.target).css('color', colorsILike[colorChoice])
   }
 
-  $('.letter').on('mouseover', changeColor);
-  $('.letter').on('mouseout', originalColor);
+  const originalColor = () => {
+    setTimeout(function() {
+      $('.letter').css('color', '#3366CC')
+    },100)
+    $('.letter').css('color', 'crimson')
+  }
+
+  $('.letter').on('mouseover', changeColor)
+  $('.letter').on('mouseout', randomColor)
+  $('.letter').on('click', originalColor)
+
 
 })
 
