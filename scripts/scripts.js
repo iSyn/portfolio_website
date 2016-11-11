44a46f90530cddf5156575ffb1333c0ac6358842
Synclair Wang <synclairwang11@yahoo.com>
2016-11-11 00:42:27 -0500

    ugh this took way too long

--- scripts/scripts.js
+++
@@ -1,5 +0,0 @@
-//test
-$(() => {
-  console.log('document is ready')
-
-})
---
+++ scripts/updateSubheader.js
@@ -0,0 +1,49 @@
+$(() => {
+
+  console.log('updateSubheader.js is linked')
+
+  let subheaderTextNum = 1;
+  let subheaderColorNum = 1;
+
+  const subheaderTextArr = [
+    'developer',
+    'eagle scout',
+    'powerlifter',
+    'dragon boater',
+  ]
+
+  const subheaderColorArr = [
+    '#F56857',
+    '#3884C1',
+    '#F89387',
+    '#1BB1B2',
+    '#F97206',
+  ]
+
+  const updateSubheader = (() => {
+
+    setInterval(() => {
+      console.log(subheaderTextNum)
+      if (subheaderTextNum >= subheaderTextArr.length) {
+        subheaderTextNum = 0;
+      }
+      if (subheaderColorNum >= subheaderColorArr.length) {
+        subheaderColorNum = 0;
+      }
+
+      $('.part-one-subheader').text(subheaderTextArr[subheaderTextNum])
+      $('.part-one-subheader').css('color', subheaderColorArr[subheaderColorNum])
+      $('.part-one-subheader').addClass('animated flipInX').one('animationend', function() {
+        $(this).removeClass('animated flipInX')
+      })
+
+      subheaderTextNum++
+      subheaderColorNum++
+
+    }, 1800)
+  })
+
+
+  updateSubheader()
+
+})
