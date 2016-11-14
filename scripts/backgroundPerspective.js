02790c19f8e17bb90c521700a6e4545dd0948aaa
Synclair Wang <synclairwang11@yahoo.com>
2016-11-14 14:35:40 -0500

    perspective semi working. stuck on right side for some reason

--- scripts/backgroundPerspective.js
+++ scripts/backgroundPerspective.js
@@ -11,17 +11,18 @@ $(() => {
       var middleX = window.innerWidth / 2;
       var middleY = window.innerHeight / 2;
 
-      $background.css('transform', `perspective(1000px)
-                                    rotateX(${(-1)*(middleY - event.clientY)/2}deg)
-                                    rotateY(${(middleX = event.clientX)/2}deg)`)
+      $background.css({
+        top: '50%',
+        left: '50%',
+        transform:
+        `
+          perspective(1000px)
+          rotateX(${(-1)*(middleY - event.clientY)/90}deg)
+          rotateY(${(middleX = event.clientX)/160}deg)
+          translate(-50%, -50%)
+        `,
+      });
     })
   }
-
-
-  window.addEventListener('resize', function(event) {
-    console.log('resize function running')
-    $background.style.height   = `${window.innerHeight}px`;
-    $background.style.width    = `${window.innerWidth}px`;
-  });
 
 });
