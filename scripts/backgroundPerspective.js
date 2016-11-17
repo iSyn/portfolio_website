0f785a32eb01b2845f22894b3154da4da73ba686
Synclair Wang <synclairwang11@yahoo.com>
2016-11-16 20:26:59 -0500

    working on perspective thing;

--- scripts/backgroundPerspective.js
+++ scripts/backgroundPerspective.js
@@ -2,27 +2,23 @@ $(() => {
 
   console.log('backgroundPerspective.js is linked')
 
-  const $background = $('.part-one-header-wrapper')
-  const $body = $('body');
-
   if (navigator.vendor.indexOf('Apple') < 0) {
-    document.addEventListener('mousemove', function (event) {
+    $(document).mousemove((e) => {
+      console.log('mousemove')
 
-      var middleX = window.innerWidth / 2;
-      var middleY = window.innerHeight / 2;
+      var middleX = window.innerWidth / 3;
+      var middleY = window.innerHeight / 3;
 
-      $background.css({
-        top: '50%',
-        left: '50%',
+      $('.part-one-header-wrapper').css({
         transform:
         `
           perspective(1000px)
-          rotateX(${(-1)*(middleY - event.clientY)/90}deg)
-          rotateY(${(middleX = event.clientX)/160}deg)
-          translate(-50%, -50%)
+          rotateX(${(-1)*(middleY - e.clientY)/90}deg)
+          rotateY(${(middleX - e.clientX)/160}deg)
         `,
       });
     })
   }
 
 });
+
