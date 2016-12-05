31c02cccfcf1c7f7269af99fe2a0ca8e9a51ded2
Synclair Wang <synclairwang11@yahoo.com>
2016-12-05 11:34:44 -0500

    idk why they didnt do this shit earlier but i hope this finally works

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,13 +2,14 @@ $(() => {
 
 
   // ###################################################################
+  // Toggles the flipped class
 
   $('.flip-click').click(() => {
     $('.card').toggleClass('flipped');
   })
 
   // ###################################################################
-
+  // Window perspective shit
   // scrapped from BobbyDigital https://github.com/gittheking
 
   if (navigator.vendor.indexOf('Apple') < 0) {
@@ -38,5 +39,87 @@ $(() => {
   }
 
   // ###################################################################
+  // Header scripts
+
+  let subheaderTextNum = 1;
+  let subheaderColorNum = 1;
+
+  const subheaderTextArr = [
+    '— developer —',
+    '— eagle scout —',
+    '— powerlifter —',
+    '— dragon boater —',
+  ]
+
+  const subheaderColorArr = [
+    '#F56857',
+    '#3884C1',
+    '#F89387',
+    '#1BB1B2',
+    '#F97206',
+  ]
+
+  const updateSubheader = (() => {
+
+    setInterval(() => {
+      if (subheaderTextNum >= subheaderTextArr.length) {
+        subheaderTextNum = 0;
+      }
+      if (subheaderColorNum >= subheaderColorArr.length) {
+        subheaderColorNum = 0;
+      }
+
+      $('.front-subheader').text(subheaderTextArr[subheaderTextNum])
+      $('.front-subheader').css('color', subheaderColorArr[subheaderColorNum])
+      $('.front-subheader').addClass('animated flipInX').one('animationend', function() {
+        $(this).removeClass('animated flipInX')
+      })
+
+      subheaderTextNum++
+      subheaderColorNum++
+
+    }, 1500)
+  })
+
+
+  updateSubheader()
+
+  // colors
+
+  const changeColor = (e) => {
+    $(e.target).css('color', 'crimson')
+  }
+
+  const randomColor = (e) => {
+    const colorsILike = [
+      'deepskyblue',
+      'cyan',
+      'turquoise',
+      'mediumturquoise',
+      'aqua',
+      'darkturquoise',
+      'skyblue',
+      'lightskyblue',
+      'steelblue',
+      'dodgerblue',
+      'cornflowerblue',
+      'royalblue',
+      'midnightblue',
+      'navy',
+    ]
+    let colorChoice = Math.floor(Math.random() * colorsILike.length)
+    $(e.target).css('color', colorsILike[colorChoice])
+  }
+
+  const originalColor = () => {
+    setTimeout(function() {
+      $('.letter').css('color', '#FDFDFD')
+    },100)
+    $('.letter').css('color', 'crimson')
+  }
+
+  $('.letter').on('mouseover', changeColor)
+  $('.letter').on('mouseout', randomColor)
+  $('.letter').on('click', originalColor)
 
 })
