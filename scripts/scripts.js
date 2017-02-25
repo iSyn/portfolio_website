5dd293b6ff54dad047328057d6f29ae96756d3dc
Synclair Wang <synclairwang11@yahoo.com>
2017-02-25 02:36:53 -0500

    basic site down

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,23 +7,4 @@ $(() => {
     showCursor: false
   });
 
-  let text = [
-    'responsive designs.',
-    'fluid UX.',
-  ]
-
-  let textCount = 0
-
-  setInterval(() => {
-    console.log(textCount)
-    if (textCount >= text.length-1) {
-      textCount = 0
-      $('.about-me-text').text('responsive designs.')
-    } else {
-      textCount++
-      $('.about-me-text').text(text[textCount])
-    }
-  }, 1500)
-
-
 })
