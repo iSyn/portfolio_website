d0f76f5c3fc02b27d8fd6dc72ad47b1fc660c332
Synclair Wang <synclairwang11@yahoo.com>
2017-02-26 14:32:32 -0500

    added gif in doors page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,23 +7,23 @@ $(() => {
     showCursor: false
   });
 
-  let doorPics = [
-    './assets/doors1.png',
-    './assets/doors2.png',
-    './assets/doors3.png',
-  ]
+  // let doorPics = [
+  //   './assets/doors1.png',
+  //   './assets/doors2.png',
+  //   './assets/doors3.png',
+  // ]
 
-  let counter = 0
+  // let counter = 0
 
-  setInterval(() => {
-    if (counter < doorPics.length - 1) {
-      counter++
-      $('.door-images').attr('src', doorPics[counter]);
-    } else {
-      counter = 0;
-      $('.door-images').attr('src', doorPics[counter]);
-    }
-  }, 1000)
+  // setInterval(() => {
+  //   if (counter < doorPics.length - 1) {
+  //     counter++
+  //     $('.door-images').attr('src', doorPics[counter]);
+  //   } else {
+  //     counter = 0;
+  //     $('.door-images').attr('src', doorPics[counter]);
+  //   }
+  // }, 1000)
 
 
 })
