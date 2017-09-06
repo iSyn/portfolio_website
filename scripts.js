09067e9dca4d88af0eadb1923514a340d82a583f
Synclair Wang <synclairwang11@yahoo.com>
2017-09-05 21:38:49 -0400

    edited styles

--- scripts.js
+++ scripts.js
@@ -4,6 +4,9 @@ let pageLoaded = () => {
     let win = window.open('https://isyn.github.io/More-Ore/', '_blank')
     win.focus
   })
+
+  let header = document.querySelector('.dev-journal-header')
+
 }
 
 
