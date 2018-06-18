59b9fe753f2a3538d127937a34e4119eb53f5664
Synclair Wang <synclairwang11@yahoo.com>
2018-06-17 21:00:04 -0400

    changed some js values to fire off an emote right away

--- professional.js
+++ professional.js
@@ -7,8 +7,7 @@ let redirect = () => {
     win.focus();
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('loaded')
+let randomExpression = () => {
 
     let expressions = [
         'wiggle eyebrows',
@@ -24,52 +23,59 @@ window.addEventListener('DOMContentLoaded', () => {
     let ears = document.querySelectorAll('.ear')
     let neck = document.querySelector('.neck')
 
+    let num = Math.floor(Math.random() * expressions.length);
+    let selected = expressions[num]
+
+    if (selected === 'wiggle eyebrows') {
+        eyebrows.forEach(eyebrow => eyebrow.classList.add('wiggle-eyebrows'))
+    } else if (selected === 'shocked') {
+        eyebrows.forEach(eyebrow => eyebrow.classList.add('shocked-eyebrows'))
+        eyelids.forEach(eyelid => eyelid.classList.add('shocked-eyelids'))
+        mouth.classList.add('shocked-mouth')
+    } else if (selected === 'curious') {
+        eyebrows[0].classList.add('curious-eyebrows-left')
+        eyebrows[1].classList.add('curious-eyebrows-right')
+        mouth.classList.add('curious-mouth')
+    } else if (selected === 'angry') {
+        eyebrows[0].classList.add('angry-eyebrows-left')
+        eyebrows[1].classList.add('angry-eyebrows-right')
+        mouth.classList.add('angry-mouth')
+        head.classList.add('angry-head')
+        ears.forEach(ear => ear.classList.add('angry-skin'))
+        eyelids.forEach(eyelid => eyelid.classList.add('angry-eyelids'))
+        neck.classList.add('angry-skin')
+    }
+
+    setTimeout(() => {
+        eyebrows.forEach(eyebrow => {
+            eyebrow.classList.remove('wiggle-eyebrows')
+            eyebrow.classList.remove('curious-eyebrows-right')
+            eyebrow.classList.remove('curious-eyebrows-left')
+            eyebrow.classList.remove('angry-eyebrows-left')
+            eyebrow.classList.remove('angry-eyebrows-right')
+            eyebrow.classList.remove('shocked-eyebrows')
+        })
+        eyelids.forEach(eyelid => {
+            eyelid.classList.remove('shocked-eyelids')
+            eyelid.classList.remove('angry-eyelids')
+        })
+        mouth.classList.remove('shocked-mouth')
+        mouth.classList.remove('curious-mouth')
+        mouth.classList.remove('angry-mouth')
+        head.classList.remove('angry-head')
+        ears.forEach(ear => ear.classList.remove('angry-skin'))
+        neck.classList.remove('angry-skin')
+
+    }, 5500)
+
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        randomExpression()
+    }, 100)
+
     setInterval(() => {
-        let num = Math.floor(Math.random() * expressions.length);
-        let selected = expressions[num]
-
-        if (selected === 'wiggle eyebrows') {
-            eyebrows.forEach(eyebrow => eyebrow.classList.add('wiggle-eyebrows'))
-        } else if (selected === 'shocked') {
-            eyebrows.forEach(eyebrow => eyebrow.classList.add('shocked-eyebrows'))
-            eyelids.forEach(eyelid => eyelid.classList.add('shocked-eyelids'))
-            mouth.classList.add('shocked-mouth')
-        } else if (selected === 'curious') {
-            eyebrows[0].classList.add('curious-eyebrows-left')
-            eyebrows[1].classList.add('curious-eyebrows-right')
-            mouth.classList.add('curious-mouth')
-        } else if (selected === 'angry') {
-            eyebrows[0].classList.add('angry-eyebrows-left')
-            eyebrows[1].classList.add('angry-eyebrows-right')
-            mouth.classList.add('angry-mouth')
-            head.classList.add('angry-head')
-            ears.forEach(ear => ear.classList.add('angry-skin'))
-            eyelids.forEach(eyelid => eyelid.classList.add('angry-eyelids'))
-            neck.classList.add('angry-skin')
-        }
-
-        setTimeout(() => {
-            eyebrows.forEach(eyebrow => {
-                eyebrow.classList.remove('wiggle-eyebrows')
-                eyebrow.classList.remove('curious-eyebrows-right')
-                eyebrow.classList.remove('curious-eyebrows-left')
-                eyebrow.classList.remove('angry-eyebrows-left')
-                eyebrow.classList.remove('angry-eyebrows-right')
-                eyebrow.classList.remove('shocked-eyebrows')
-            })
-            eyelids.forEach(eyelid => {
-                eyelid.classList.remove('shocked-eyelids')
-                eyelid.classList.remove('angry-eyelids')
-            })
-            mouth.classList.remove('shocked-mouth')
-            mouth.classList.remove('curious-mouth')
-            mouth.classList.remove('angry-mouth')
-            head.classList.remove('angry-head')
-            ears.forEach(ear => ear.classList.remove('angry-skin'))
-            neck.classList.remove('angry-skin')
-
-        }, 5500)
-
-
+        randomExpression()
     }, 6.5 * 1000)
 })
